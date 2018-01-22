9f6d983f46f81a12a99072a4570df1aa22d4de02
����Ⱥ <sunyaqun@heshine.com>
2018-01-22 09:36:13 +0800

    获取评论数据

--- src/components/subcom/comment.vue
+++ src/components/subcom/comment.vue
@@ -1,28 +1,28 @@
 <!-- 评论组件 -->
 <template>
+  <div id="postcomment">
+    <!-- 1.0实现提交评论数据到服务的静态结构 -->
     <div id="postcomment">
-        <!-- 1.0实现提交评论数据到服务的静态结构 -->
-        <div id="postcomment">
 
-            <h3>提交评论</h3>
-            <hr>
-            <textarea placeholder="请输入您要评论的内容" v-model="postconent"></textarea>
-            <mt-button type="primary" size="large" @click="postcomment()">发表</mt-button>
-        </div>
-        <!-- 2.0评论数据列表 -->
-        <h3>评论列表</h3>
-        <hr>
-        <div v-for="(item,index) in list" :key="item.id">
-            <div class="title">
-                <span>第{{index+1}}楼:</span>
-                <span>用户:{{item.author}}</span>
-                <span>发表时间:{{item.time*1000 | changetime}}</span>
-            </div>
-            <ul class="mui-table-view">
-                <li class="mui-table-view-cell">{{item.content}}</li>
-            </ul>
-        </div>
+      <h3>提交评论</h3>
+      <hr>
+      <textarea placeholder="请输入您要评论的内容" v-model="postconent"></textarea>
+      <mt-button type="primary" size="large" @click="postcomment()">发表</mt-button>
     </div>
+    <!-- 2.0评论数据列表 -->
+    <h3>评论列表</h3>
+    <hr>
+    <div v-for="(item,index) in list" :key="item.id">
+      <div class="title">
+        <span>第{{index+1}}楼:</span>
+        <span>用户:{{item.author}}</span>
+        <span>发表时间:{{item.time*1000 | changetime}}</span>
+      </div>
+      <ul class="mui-table-view">
+        <li class="mui-table-view-cell">{{item.content}}</li>
+      </ul>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -35,36 +35,7 @@ export default {
   data() {
     return {
       postconent: "", //用来自动获取用户填写的评论内容
-      list: [
-        {
-          author: "朔方之月",
-          content:
-            "18岁时觉得30岁的人老得半截进了土，现在37了，反而觉得越活越通透，心态越来越淡定，越明白什么重要什么不重要。目前正在努力在职业生涯上一个台阶，照顾好家人和孩子，生活充满希望",
-          avatar: "http://pic1.zhimg.com/da8e974dc_im.jpg",
-          time: 1516346925,
-          id: 31015285,
-          likes: 2
-        },
-        {
-          author: "王二锤",
-          content: "26岁症状全有怎么办",
-          avatar:
-            "http://pic1.zhimg.com/v2-4a72e1ff20f083057d61dc3338ae1d94_im.jpg",
-          time: 1516346241,
-          id: 31015241,
-          likes: 4
-        },
-        {
-          author: "蒋龙峰",
-          content:
-            "焦虑症大家或多或少都有，可能是亲情，爱情，友情，权利，金钱等。刚刚工作的时候的领导送了我三句话，当我茫然失措的时候我就问自己，现在送给你们：你有什么？你想得到什么？你能放弃什么？",
-          avatar:
-            "http://pic4.zhimg.com/5846d0a1d77880557133cb18ab1dcc1b_im.jpg",
-          time: 1516345990,
-          id: 31015230,
-          likes: 3
-        }
-      ]
+      list: []//评论获取
     };
   },
 
@@ -100,19 +71,17 @@ export default {
       // 提交内容后清空内容
       this.postconent = "";
     },
-    // 获取数据
-    getlist() {
+    // 获取数据pageindex当前获取的是那一页的数据，默认值为1
+    getlist(pageindex) {
+      // 设默认值
+      pageindex = pageindex | 1;
       axios
         .get(
           API_PROXY +
-            common.apidomain +
-            "/api/4/story/" +
-            this.id +
-            "/short-comments"
+            common.apidomain +"/api/4/story/" +this.id +'/short-comments'
         )
         .then(res => {
-          console.log(this.id);
-          console.log(res.data);
+          this.list=res.data.comments
         });
     }
   }
